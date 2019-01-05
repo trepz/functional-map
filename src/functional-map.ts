1649cf11e0ec8e38cb808cb769cbfc7289dce6c6
Robert Worman <tr3ppy@gmail.com>
2019-01-05 16:30:20 +1000

    Add thisArg and bind for completeness

--- src/functional-map.ts
+++ src/functional-map.ts
@@ -3,8 +3,10 @@ export default class FMap<K, V> extends Map<K, V> {
         super(entries)
     }
 
-    map<T>(fn: (value: V, key: K, map: FMap<K, V>) => T): FMap<K, T> {
+    map<T>(fn: (value: V, key: K, map: FMap<K, V>) => T, thisArg?: any): FMap<K, T> {
         const nMap = new FMap<K, T>()
+        if (thisArg) fn = fn.bind(thisArg)
+
         for (let [k, v] of this) {
             nMap.set(k, fn(v, k, this))
         }
