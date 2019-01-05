94e9bb8b2fecc2a0f537e1ee81fcd4bc65fca5fb
Robert Worman <tr3ppy@gmail.com>
2019-01-05 16:23:43 +1000

    Add the original Map as third param in map callback

--- src/functional-map.ts
+++ src/functional-map.ts
@@ -3,10 +3,10 @@ export default class FMap<K, V> extends Map<K, V> {
         super(entries)
     }
 
-    map<T>(fn: (value: V, key: K) => T): FMap<K, T> {
+    map<T>(fn: (value: V, key: K, map: FMap<K, V>) => T): FMap<K, T> {
         const nMap = new FMap<K, T>()
         for (let [k, v] of this) {
-            nMap.set(k, fn(v, k))
+            nMap.set(k, fn(v, k, this))
         }
         return nMap
     }
